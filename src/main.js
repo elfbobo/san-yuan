ffb74a2ce833691cfdce67458958a3c6a5f7fb5f
zzh <1002723879@qq.com>
2019-10-08 15:17:15 +0800

    remove video.js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
-import videoJs from 'video.js'
-import 'video.js/dist/video-js.css'
 
 Vue.config.productionTip = false
-Vue.prototype.$video = videoJs
 new Vue({
   store,
   render: h => h(App)
