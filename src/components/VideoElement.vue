58201a6bb74b80cff6257f425c601606a8833361
zzh <1002723879@qq.com>
2019-10-12 09:49:36 +0800

    all finish

--- src/components/VideoElement.vue
+++ src/components/VideoElement.vue
@@ -1,40 +1,24 @@
 <template>
-  <video
-    class="video-element"
-    ref="video"
-    :controls="controls"
-    :loop="loop"
-    :autoplay="autoplay"
-    :muted="muted"
-  >
-    <source :src="videoSrc">
-  </video>
+ <video
+   ref="video"
+   class="video-element"
+   :controls="controls"
+   autoplay
+   muted
+   loop
+ >
+   <source :src="videoSrc" />
+ </video>
 </template>
 
 <script>
 import xczxg1 from '../assets/xczxg1.mp4'
 export default {
   name: 'VideoElement',
-  props: {
-    videoSrc: {
-      type: [Object, Boolean, String],
-      default: xczxg1 || 'http://cdn.lagabu.com/xczxg1.mp4'
-    },
-    controls: {
-      type: [Object, Boolean, String],
-      default: false
-    },
-    loop: {
-      type: [Object, Boolean, String],
-      default: 'loop'
-    },
-    autoplay: {
-      type: [Object, Boolean, String],
-      default: 'autoplay'
-    },
-    muted: {
-      type: [Object, Boolean, String],
-      default: 'muted'
+  data () {
+    return {
+      videoSrc: xczxg1,
+      controls: false
     }
   }
 }
